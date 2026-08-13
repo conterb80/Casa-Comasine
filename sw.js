ce82905b0f0d92352245089b007e716704b094f0
conterb80 <conterb80@gmail.com>
2026-08-14 00:41:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='casa-comasine-rc9-1-navigation-fix';
+const CACHE='casa-comasine-rc10-sentieri-reali-import';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
